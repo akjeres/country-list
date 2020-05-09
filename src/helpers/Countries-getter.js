0573a3cbfae87497002ba5870c5ea69d8bc6a2c7
akjeres <akjeres@gmail.com>
2020-05-10 01:23:34 +0300

    Requests getter switched to axios

--- src/helpers/Countries-getter.js
+++ src/helpers/Countries-getter.js
@@ -1,29 +1,18 @@
+const axios = require('axios');
 export default class CountriesGetter {
     _base = 'https://countries.trevorblades.com';
 
     get = async (body) => {
-        const res = await fetch(this._base, {
-            "headers": {
-                "accept": "*/*",
-                "accept-language": "ru,en-US;q=0.9,en;q=0.8,uk;q=0.7",
-                "cache-control": "no-cache",
-                "content-type": "application/json",
-                "pragma": "no-cache",
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-origin"
-            },
-            "referrer": this._base,
-            "referrerPolicy": "no-referrer-when-downgrade",
-            "body": body,
-            "method": "POST",
-            "mode": "cors"
-        });
+        try {
+            const res = await axios.post(
+                this._base,
+                {...JSON.parse(body)}
+            );
 
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${this._base}, received ${res.status}`);
+            return res.data;
+        } catch (error) {
+            console.error(error);
+            throw new Error(`Could not fetch ${this._base}`);
         }
-
-        return res.json();
     }
 }
