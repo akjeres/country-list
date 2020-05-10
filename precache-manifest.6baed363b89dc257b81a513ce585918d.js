5e15f4a470a75a8799a85937cb9a358ffd72758a
akjeres <akjeres@gmail.com>
2020-05-10 07:30:54 +0300

    Updates

---
+++ precache-manifest.6baed363b89dc257b81a513ce585918d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19156659e4defe03d6e1f32809ae6458",
+    "url": "/country-list/index.html"
+  },
+  {
+    "revision": "d2e4220ed90828663ea8",
+    "url": "/country-list/static/css/main.8c40bacb.chunk.css"
+  },
+  {
+    "revision": "1009ee42775d87b389ad",
+    "url": "/country-list/static/js/2.609ef319.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/country-list/static/js/2.609ef319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e4220ed90828663ea8",
+    "url": "/country-list/static/js/main.1be707eb.chunk.js"
+  },
+  {
+    "revision": "05bbea44971059da024e",
+    "url": "/country-list/static/js/runtime-main.2eb5db74.js"
+  }
+]);
